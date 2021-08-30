9963a81a7922f90735d975a0f36a31bc608f33f6
Jose Ricardo Silveira <ricardusilveira@gmail.com>
2021-08-29 21:39:32 -0300

    Adicionado algumas funções de diretorios e renomeador, nao funciona

--- principal.py
+++ principal.py
@@ -7,16 +7,14 @@ Created on Sat Aug 21 23:24:57 2021
 """
 
 
-from os.path import dirname, abspath
+from os.path import dirname
 import os
-print(dirname(abspath(__file__)))
-print(dirname(__file__))
-print(abspath(__file__))
+from typing import List
 diretorio_local = dirname(__file__)
 lista_diretorios = [x for x in os.walk(diretorio_local)]
 
 
-class Minuscularizador:
+class Renomeador:
     """Pega o caminho de um diretório ou arquivo e renomeia para minúsculo.
 
 
@@ -26,9 +24,9 @@ class Minuscularizador:
     Atributos
     """
 
-    def __init__(self, diretorio) -> None:
+    def __init__(self, diretorio_absoluto: str, caracteres_removiveis: str, caracteres_novos: str = '') -> None:
         """
-        Inicializa a classe Minuscularizador com o nome do diretório.
+        Inicializa a classe Renomeador com o nome do diretório.
 
         Argumentos:
             diretorio
@@ -37,7 +35,9 @@ class Minuscularizador:
         None.
 
         """
-        self.diretorio = self.__valida_diretorio(diretorio)
+        self.__diretorio_raiz = self.__valida_diretorio(diretorio_absoluto)
+        self.__caracteres_removiveis = caracteres_removiveis
+        self.__caracteres_novos = caracteres_novos
 
     @staticmethod
     def __valida_diretorio(diretorio_para_validar: str) -> str:
@@ -46,6 +46,53 @@ class Minuscularizador:
         else:
             raise ValueError("Não foi informado um diretório válido.")
 
+    @property
+    def get_diretorio_raiz(self) -> str:
+        return self.__diretorio_raiz
+
+    @staticmethod
+    def armazena_diretorios(arquivos_diretorios) -> List[str]:
+        return [
+            x for x in os.listdir(arquivos_diretorios) if not (
+                x.startswith('.') or x.endswith(('.md', '.py', '.sh', '.cfg', '.ci'))
+            )
+        ]
+
+    def lista_arquivos_renomeia(self, diretorio_pai: str) -> List[str]:
+        todos_arquivos = []
+        for entrada in self.armazena_diretorios(diretorio_pai):
+            caminho_completo = os.path.join(diretorio_pai, entrada)
+            if os.path.isdir(caminho_completo):
+                self.renomeia_arquivo(diretorio_pai, entrada)
+                todos_arquivos.append(self.lista_arquivos_renomeia(caminho_completo))
+            else:
+                self.renomeia_arquivo(diretorio_pai, entrada)
+                todos_arquivos.append(caminho_completo)
+        return todos_arquivos
+
+    def renomeia_arquivo(self, diretorio_pai, nome_antigo: str) -> None:
+        if len(self.get_caracteres_removiveis.strip()) == 0:
+            return None
+        else:
+            for caracter in self.get_caracteres_removiveis():
+                os.rename(
+                    os.join(diretorio_pai, nome_antigo),
+                    os.join(diretorio_pai, nome_antigo.replace(caracter, self.get_caracteres_novos))
+                )
+
+    @property
+    def get_caracteres_removiveis(self):
+        return self.__caracteres_removiveis
+
+    @property
+    def get_caracteres_novos(self):
+        return self.__caracteres_novos
+
 
 if __name__ == '__main__':
-    Minuscularizador('/run/media/ricardo/samsung1tb/google_drive/projetos/renomeador/')
+    # pass
+    # Minuscularizador('/run/media/ricardo/samsung1tb/google_drive/projetos/renomeador/')
+    # ? Precisa verificar qual SO ou estabelecer função que funcione independente do SO
+    todos_arquivos = Renomeador(r"F:/google_drive/projetos/renomeador/", '&()').lista_arquivos_renomeia(
+        r"F:/google_drive/projetos/renomeador/")
+    # print(todos_arquivos)
